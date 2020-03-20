726afca50fd3882a1858259bef86acb4143d8e6e
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2020-03-20 16:15:57 +0900

    first template smaple

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # text-template-sample
+
+template sample
+
+```
+--------
+template sring is {{ 1 }}
+1
+--------
+--------
+template sring is hoge  {{- "fuga" -}}  hoge
+hogefugahoge
+--------
+--------
+template sring is {{ if "hoge" }} fuga {{ end }}
+ fuga
+--------
+--------
+template sring is {{ range . }} {{.}}hoge {{ end }}
+ 1hoge  2hoge  3hoge  4hoge
+--------
+--------
+template sring is {{define "T1"}}ONE{{end }}{{template "T1" }}{{template "T1" }}
+ONEONE
+--------
+--------
+template sring is {{define "T1"}} {{.}} ONE {{end }}{{template "T1" "ZERO"}}
+ ZERO ONE
+--------
+--------
+template sring is {{block "T1" "ZERO"}} {{.}} ONE {{end }}
+ ZERO ONE
+--------
+--------
+template sring is {{with "ZERO"}} {{.}} ONE {{end}}
+ ZERO ONE
+--------
+--------
+template sring is {{$a := "hoge"}}{{$a}}
+hoge
+--------
+--------
+template sring is {{range $key, $value := .}}key:{{$key}},value:{{$value}}
+{{end}}
+key:foo,value:bar
+key:hoge,value:fuga
+
+--------
+```
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+func main() {
+
+	type TemplateSet struct {
+		templateStr string
+		data        interface{}
+	}
+
+	cases := []TemplateSet{
+		TemplateSet{"{{ 1 }}", nil},
+		TemplateSet{`hoge  {{- "fuga" -}}  hoge`, nil},
+		TemplateSet{`{{ if "hoge" }} fuga {{ end }}`, nil},
+		TemplateSet{`{{ range . }} {{.}}hoge {{ end }}`, []int{1, 2, 3, 4}},
+		TemplateSet{`{{define "T1"}}ONE{{end }}{{template "T1" }}{{template "T1" }}`, nil},
+		TemplateSet{`{{define "T1"}} {{.}} ONE {{end }}{{template "T1" "ZERO"}}`, nil},
+		TemplateSet{`{{block "T1" "ZERO"}} {{.}} ONE {{end }}`, nil},
+		TemplateSet{`{{with "ZERO"}} {{.}} ONE {{end}}`, nil},
+		TemplateSet{`{{$a := "hoge"}}{{$a}}`, nil},
+		TemplateSet{`{{range $key, $value := .}}key:{{$key}},value:{{$value}}
+{{end}}`, map[string]string{"hoge": "fuga", "foo": "bar"}},
+	}
+
+	for _, t := range cases {
+		tmpl, err := template.New("test").Parse(t.templateStr)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("--------")
+		fmt.Printf("template sring is %s\n", t.templateStr)
+		err = tmpl.Execute(os.Stdout, t.data)
+		fmt.Println("\n--------")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
